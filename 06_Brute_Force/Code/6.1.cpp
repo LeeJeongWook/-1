0c19ab09cd73c16b98b090f563fdef3b850230c1
leejw0709@gmail.com <leejw0709@gmail.com>
2022-03-17 23:59:51 +0900

    -

--- 06_Brute_Force/Code/6.1.cpp
+++ 06_Brute_Force/Code/6.1.cpp
@@ -2,16 +2,26 @@
 
 using namespace std;
 
-int c = 0;
-int d = 100;
+// 필수 조건: n >= 1
+// 결과: 1부터 n까지의 합을 반환한다.
+int sum(int n){
+    int ret = 0;
+    for(int i = 1; i <= n; ++i)
+        ret += i;
+    return ret;
+}
+// 필수 조건: n >= 1
+// 결과: 1부터 n까지의 합을 반환한다.
+int recursiveSum(int n){
+    if(n==1) return 1;  // 더 이상 쪼개지지 않을 때
+    return n + recursiveSum(n-1);
+}
 
-int main()
-{
-    int a = 2;
-    int b = 2;
-    int c = a + b;
+int main(){
+    int n = 10;
 
-    cout << a << " + " << b << " = " << c << endl;
+    cout << sum(n) << endl;
+    cout << recursiveSum(n) << endl;
 
     return 0;
 }
