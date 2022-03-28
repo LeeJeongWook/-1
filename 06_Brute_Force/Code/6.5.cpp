4d45b3afee32318c91da59c977e91e5954282195
leejw0709@gmail.com <leejw0709@gmail.com>
2022-03-28 23:31:43 +0900

    -

--- 06_Brute_Force/Code/6.5.cpp
+++ 06_Brute_Force/Code/6.5.cpp
@@ -1,97 +1,57 @@
-//6.5 문제: 게임판 덮기 (문제 ID: BOARDCOVER, 난이도 : 하)
-
 #include <iostream>
-#include <cassert>
-#include <vector>
-#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
-// 주어진 칸을 덮을 수 있는 4가지 방법
-// 블록을 구성하는 3칸의 상대적 위치 (dy,dx) 의 목록
-const int coverType[4][3][2] = {
-  { { 0, 0 }, { 1, 0 }, { 0, 1 } },
-  { { 0, 0 }, { 0, 1 }, { 1, 1 } },
-  { { 0, 0 }, { 1, 0 }, { 1, 1 } },
-  { { 0, 0 }, { 1, 0 }, { 1, -1 } }};
+int n;
+bool friends[10][10];
 
-/*
-board의 (y, x)를 type번 방법으로 덮거나, 덮었던 블록을 없앤다.
-delta = 1 이면 덮고, -1이면 덮었던 블록을 없앤다.
-만약 블록이 제대로 덮이지 않은 경우
-(게임판 밖으로 나가거나, 겹치거나 검은 칸을 덮을 때) false를 반환단다.
-*/
-bool set(vector<vector<int>>&board, int y, int x, int type, int delta){
-  bool ok = true;
-  for(int i = 0; i < 3; ++i){
-    const int ny = y + coverType[type][i][0];
-    const int nx = x + coverType[type][i][1];
-    if(ny < 0 || ny >= board.size() || nx < 0 || nx >= board[0].size())
-      ok = false;
-    else if((board[ny][nx] += delta) > 1)
-      ok = false;
-  }
-  return ok;
-}
-
-// board의 모든 빈 칸을 덮을 수 있는 방법의 수를 반환한다.
-// board[i][j] = 1 이미 덮인 칸 혹은 검은 칸
-// board[i][j] = 0 아직 덮이지 않은 칸
-int cover(vector<vector<int>>& board){
-  // 아직 채우지 못한 칸 중 가장 윗줄 왼쪽에 있는 칸을 찾는다.
-  int y = -1, x = -1;
-  for(int i = 0; i < board.size(); ++i){
-    for(int j = 0; j < board[i].size(); ++j){
-      if(board[i][j] == 0){
-        y = i;
-        x = j;
-        break;
-      }
+// taken[i] = i번째 학생이 짝을 이미 찾았으면 true, 아니면 false
+int countPairings(bool taken[10]){
+    // 남은 학생들 중 가장 번호가 빠른 학생을 찾는다.
+    int firstFree = -1;
+    for(int i = 0; i < n; ++i){
+        if(!taken[i]){
+            firstFree = i;
+            break;
+        }
     }
-    if(y != -1) break;
-  }
-  // 기저 사례: 모든 칸을 채웠으면 1을 반환한다.
-  if(y == -1) return 1;
-  int ret = 0;
-  for(int type = 0; type < 4; ++type){
-    // 만약 board[y][x]를 type 형태로 덮을 수 있으면 재귀 호출한다.
-    if(set(board, y, x, type, 1))
-      ret += cover(board);
-    // 덮었던 블록을 치운다.
-    set(board, y, x, type, -1);
-  }
-  return ret;
+    //기저 사례: 모든 학생이 짝을 찾았으면 한 가지 방법을 찾았으니 종료한다.
+    if(firstFree == -1){
+        return 1;
+    }
+    int ret = 0;
+    // 현재 학생과 짝지을 학생을 결정한다.
+    for(int pairWith = firstFree+1; pairWith < n; ++pairWith){
+        if(!taken[pairWith] && friends[firstFree][pairWith]){
+            taken[firstFree] = taken[pairWith] = true;
+            ret += countPairings(taken);
+            taken[firstFree] = taken[pairWith] = false;
+        }
+    }
+    return ret;
 }
-
 int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  freopen("input.txt", "r", stdin);
-  int C;
-  cin >> C;
-  
-  for(int test_case = 0; test_case < C; test_case++){
-    int H, W;
-    cin >> H >> W;
-    assert(1 <= H && H <= 20);
-    assert(1 <= W && W <= 20);
-    vector<vector<int>> board(H, vector<int>(W, 0));
-    int whites = 0;
-    for(int i = 0; i < H; i++){
-      string s;
-      cin >> s;
-      for(int j = 0; j < W; j++){
-        if(s[j] == '#')
-          board[i][j] = 1;
-      }
-      whites += count(board[i].begin(), board[i].end(), 0);
+    freopen("input.txt", "r", stdin);
+
+    bool taken[10];
+    int C, m, idx, a, b;
+    
+    cin >> C;
+    for(int test_case = 0; test_case < C; test_case++){
+        memset(friends, 0, sizeof(friends));
+        cin >> n >> m;
+        for(int i = 0; i < m; i++){
+            cin >> a >> b;
+            friends[a][b] = friends[b][a] = 1;
+        }
+        memset(taken, 0, sizeof(taken));
+        cout << countPairings(taken) << endl;
     }
-    assert(whites <= 50);
-    int ways = cover(board);
-    cout << ways << endl;
-  }
 
-  return 0;
+    return 0;
 }
